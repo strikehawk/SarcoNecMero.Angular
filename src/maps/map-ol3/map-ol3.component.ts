764554ce2b6982ce8957b4a1a4475c728c03b6b8
Strikehawk <strikehawk@hotmail.com>
2016-12-28 11:58:57 +0100

    ajout support proj4

--- src/maps/map-ol3/map-ol3.component.ts
+++ src/maps/map-ol3/map-ol3.component.ts
@@ -1,14 +1,17 @@
 /// <reference path="../../../typings/angular/angular.d.ts" />
 /// <reference path="../../../typings/openlayers/openlayers.d.ts" />
+/// <reference path="../../../typings/proj4/proj4.d.ts" />
 
 module adunware.snm.components {
     class Controller {
         constructor() {
+            this._loadProjections();
             this._createMap("map-ol3");
         }
 
         private _createMap(elementId: string): void {
             let view: ol.View = new ol.View({
+                projection: "EPSG:2154",
                 center: [0, 0],
                 zoom: 1
             });
@@ -24,6 +27,10 @@ module adunware.snm.components {
                 view: view,
                 layers: layers
             })
+        }
+
+        private _loadProjections(): void {
+            proj4.defs("EPSG:2154", "+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
         }
     }
 
